35f8a9eb0d845e41cbd542602d0dec027212a4f8
kfchinese <larry.chiem@getcruise.com>
2019-10-29 16:47:38 -0700

    staging

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/larry.chiem/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/larry.chiem/Downloads/YUApp-Test-master"
-export "FLUTTER_TARGET=/Users/larry.chiem/Downloads/YUApp-Test-master/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/larry.chiem/Downloads/YUApp"
+export "FLUTTER_TARGET=/Users/larry.chiem/Downloads/YUApp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/larry.chiem/development/flutter/bin/cache/artifacts/engine/ios"
