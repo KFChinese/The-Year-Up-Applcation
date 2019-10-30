0f4ae26a342a0aac79db85fde0e194070d43a949
kfchinese <larry.chiem@getcruise.com>
2019-10-30 09:29:28 -0700

    Added File

---
+++ lib/UI/settings.dart
@@ -0,0 +1,11 @@
+// TODO Implement this library.
+
+import 'package:flutter/material.dart';
+class settings extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+  
+}
